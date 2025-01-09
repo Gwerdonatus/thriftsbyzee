774590a6b75da41941253c560d42ad037361959b
Gwer Donatus <donatusgwer>
2025-01-09 10:29:13 +0100

    Updated models for Cloudinary and updated templates for fallback images

--- store/admin.py
+++ store/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Dress
 
 admin.site.register(Dress)
+
+
+
--- store/models.py
+++ store/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField  # Import CloudinaryField
 
 # Create your models here.
 
 class Dress(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='dresses/')
+    image = CloudinaryField('image')  # Use CloudinaryField for images
     video = models.URLField(blank=True, null=True)  
     price = models.DecimalField(max_digits=10, decimal_places=2)
     availability = models.BooleanField(default=True)  # In stock or out of stock
